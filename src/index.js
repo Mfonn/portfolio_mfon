8ee5c7c011edec1a841473af803250fda9aa37c3
Mfon <mfonnta05@gmail.com>
2022-11-22 01:12:52 +0100

    pages4

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import './index.css'
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-    <BrowserRouter basename="https://mfonn.github.io/portfolio_mfon/">
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <App />
     </BrowserRouter>
 )
